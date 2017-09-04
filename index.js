cdaf47a068f29fdf2da7bb7d30f3300073d1b5cc
Faith <fmugure@intracitygeeks.org>
2017-09-04 03:02:53 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  var buzzer = $("#buzzer")[0];
+  var count = parseInt($("#Num").html());
+  //console.log(count);
+  $("#reset").hide();
+
+
+  $("#minus5Clock").click(function(){
+  if(count>5){
+    count -=5;
+    $("#num").html(count);
+}
+});
+$("#add5Clock").click(function(){
+    count +=5;
+    $("#num").html(count);
+});
+  $("#minus5Break").click(function(){
+  if (breakTime > 5){
+    breaktime -=5;
+    $("#breaknum").html(breakTime);
+}
+ });
+$("#add5Break").click(function(){
+    breaktime +=5;
+    $("#breakNum").html(breakTime);
+  });
+});
