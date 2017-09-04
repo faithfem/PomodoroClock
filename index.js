cc5fe375ee993c531536429efe3d9a7f78f1fdb8
Faith <fmugure@intracitygeeks.org>
2017-09-04 04:32:52 +0000

    change js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 $(document).ready(function(){
-  var buzzer = $("#buzzer")[0];
-  var count = parseInt($("#Num").html());
-  //console.log(count);
+  /*var buzzer = $("#buzzer")[0];
+  buzzer.play();*/
+  var count = 5;
+  var count = parseInt($("#breakNum").html());
+  console.log(count);
   $("#reset").hide();
-
-
-  $("#minus5Clock").click(function(){
+  
+$("#minus5Clock").click(function(){
   if(count>5){
     count -=5;
-    $("#num").html(count);
-}
-});
+    $("num").html(count);
+  
 $("#add5Clock").click(function(){
     count +=5;
     $("#num").html(count);
-});
+});  
+  
   $("#minus5Break").click(function(){
   if (breakTime > 5){
     breaktime -=5;
     $("#breaknum").html(breakTime);
-}
  });
+
 $("#add5Break").click(function(){
     breaktime +=5;
     $("#breakNum").html(breakTime);
-  });
+  });  
+   
 });
